81d453c4628bb4a28b4bd8fab4d23dbdb9acd26c
mcwaller422 <mcwaller42@gmail.com>
2019-05-06 20:25:10 +0000

    pseudo_code

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -22,4 +22,3 @@ if operator == '1'
 end
 
 puts answer 
-
---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,42 @@
+# #return the sum of two integers
+
+# START
+# GET user's two integers
+# SET variable 1 and 2
+# SET variable sum
+# PRINT sum
+# END
+
+
+
+#takes an array of strings, and returns a string that is all those strings concatenated
+
+#informal
+# set variable for array
+# set variable for new concatenated string
+# concatenated string variable should be = to method called over original array
+
+#formal
+# START
+# SET variable array
+# SET variable string
+# WHILE array is not empty
+# READ array index and push to string
+# END
+
+
+
+#takes an array of integers and returns and new array 
+
+#informal
+# set variable for array
+# set variable for new array 
+# push the data from one array to the next
+
+# #formal
+# START
+# SET variable array
+# SET variable new_array
+# READ new_array = array 
+# PRINT new_array
+# END
